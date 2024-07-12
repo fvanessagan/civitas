b8a4c9a415851c284300093f5d6fa86e8cbf92ff
DioDateUs <123476311+DioDateUs@users.noreply.github.com>
2024-07-12 16:17:46 +0700

    Update urls.py

--- civitas/main_app/urls.py
+++ civitas/main_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import generate_roadmap
 
 urlpatterns = [
     path("", views.home),
@@ -7,8 +8,9 @@ urlpatterns = [
 
     path("sign-up/", views.sign_up),
     path("account/", views.account),
-
+    
     path("learning-steps/", views.steps),
     path("learning-steps/new/", views.newlesson, name="add-lesson"),
     path("group/", views.group),
+    path('generate-roadmap/', generate_roadmap, name='generate_roadmap'),
 ]
